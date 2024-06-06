2b1699b9d8b8c037fe157b5a71533e661ace56cf
tiljrd <til.jordan@outlook.de>
2024-06-06 13:58:09 +0100

    Add log messages

--- internal/api/cloud.go
+++ internal/api/cloud.go
@@ -47,6 +47,8 @@ func loadTemplate(templatePath string) (*template.Template, error) {
 }
 
 func createIngress(data IngressData) error {
+
+	fmt.Printf("Creating ingress with the following data: %v", data)
 	config, err := clientcmd.BuildConfigFromFlags("", "/home/ubuntu/.kube/config")
 	if err != nil {
 		return err
@@ -69,12 +71,18 @@ func createIngress(data IngressData) error {
 		return err
 	}
 
+	// Print the rendered template for debugging
+	fmt.Println("Rendered Ingress YAML:")
+	fmt.Println(buf.String())
+
 	// Convert the rendered template to an unstructured object
 	ingress := &unstructured.Unstructured{}
 	dec := runtime.DecodeInto(scheme.Codecs.UniversalDeserializer(), buf.Bytes(), ingress)
 	if dec != nil {
 		return err
 	}
+
+	fmt.Printf("Creating the following ingress: %v", ingress)
 
 	resource := schema.GroupVersionResource{
 		Group:    "networking.k8s.io",
@@ -92,9 +100,11 @@ func createIngress(data IngressData) error {
 			// Ingress does not exist, create it
 			_, err = dynClient.Resource(resource).Namespace(namespace).Create(context.Background(), ingress, metav1.CreateOptions{})
 			if err != nil {
+				fmt.Printf("Error creating ingress: %v:", err)
 				return err
 			}
 		} else {
+			fmt.Printf("Ingress exists or other error: %v", err)
 			return err
 		}
 	}
