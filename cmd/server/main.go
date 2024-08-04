d2f4c19639b720c2607ccb4ef6bdebe2b5865785
tiljrd <til.jordan@outlook.de>
2024-08-04 13:53:34 +0100

    Allow Authorization header in CORS

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -29,8 +29,8 @@ func main() {
 func withCORS(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
